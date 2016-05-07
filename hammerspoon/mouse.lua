67f16baba276950dddc1398b1534682a4e54c1a2
Stephen Jennings <stephen.g.jennings@gmail.com>
2016-05-07 12:26:00 -0700

    add a comment to mouse.lua

--- hammerspoon/mouse.lua
+++ hammerspoon/mouse.lua
@@ -1,3 +1,11 @@
+-- I have a mouse which has back/forward buttons, but OS X doesn't interpret
+-- these as back and forward. This script listens for buttons 3 and 4 to be
+-- pressed, and posts a CMD-Left or CMD-Right command instead.
+--
+-- I've also added an item in the menu bar so I can disable this interception,
+-- just in case I run across something that this interception logic interferes
+-- with (games, maybe?).
+
 local interceptBackForward = hs.eventtap.new({25}, function(evt)
     local key = evt:getProperty(3)
     if key == 3 then
