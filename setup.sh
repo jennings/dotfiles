5ec4f43d4217b2e10d7d411106308816fa1cf8f0
Stephen Jennings <stephen.g.jennings@gmail.com>
2020-10-04 20:05:37 -0700

    setup: add counts at end of process

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 usage()
 {
 cat << EOF
-usage: $0 [-n]
+usage: $1 [-n]
 
 OPTIONS:
     -n      Dry run; displays what would have been executed
@@ -18,7 +18,7 @@ while getopts "hn" OPTION; do
             DRYRUN=1
             ;;
         h|*)
-            usage
+            usage "$0"
             exit 1
     esac
 done
@@ -36,16 +36,20 @@ fi
 HERE=$(python -c "import os.path; print os.path.relpath(os.path.realpath('.'), os.path.expanduser('~'))")
 
 # For each dotfile, create ~/.filename
+dots_added=0
 for f in dotfiles/*; do
 
     fn=$(basename $f)
     # Don't overwrite a dotfile that already exists
-    [[ ( ! -e "$HOME/.$fn" ) || ( -L "$HOME/.$fn" ) ]] && $LNCMD -shf "$HERE/$f" "$HOME/.$fn"
+    [[ ( ! -e "$HOME/.$fn" ) || ( -L "$HOME/.$fn" ) ]] && $LNCMD -shf "$HERE/$f" "$HOME/.$fn" && (( dots_added++ ))
 
 done
 
 # For each binary, create ~/bin/filename
 $MKDIRCMD -p "$HOME/bin"
+bins_added=0
 for f in bin/*; do
-    [[ ( ! -e "$HOME/$f" ) || ( -L "$HOME/$f" ) ]] && $LNCMD -shf "../$HERE/$f" "$HOME/$f"
+    [[ ( ! -e "$HOME/$f" ) || ( -L "$HOME/$f" ) ]] && $LNCMD -shf "../$HERE/$f" "$HOME/$f" && (( bins_added++ ))
 done
+
+echo "  $(bin/badge -n '' -b blue dotfiles $dots_added)  $(bin/badge -b blue binaries $bins_added)"
