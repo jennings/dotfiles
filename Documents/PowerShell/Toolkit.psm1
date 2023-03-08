30fc95f40fe87caa757cba9c4798ffec5c872e52
Stephen Jennings <stephen.g.jennings@gmail.com>
2023-03-07 16:21:02 -0800

    update PowerShell profile

--- Documents/PowerShell/Toolkit.psm1
+++ Documents/PowerShell/Toolkit.psm1
@@ -1,3 +1,4 @@
+New-Alias seq Get-Sequence
 function Get-Sequence {
     if ($Args.Count -ge 2) {
         $start = $Args[0]
@@ -9,8 +10,7 @@ function Get-Sequence {
     [System.Linq.Enumerable]::Range($start, $end - $start + 1)
 }
 
-New-Alias seq Get-Sequence
-
+New-Alias xargs Invoke-Xargs
 function Invoke-Xargs {
     begin { $items = @() }
     process { $items += $_ }
@@ -35,8 +35,90 @@ function Invoke-Xargs {
     }
 }
 
-New-Alias xargs Invoke-Xargs
+New-Alias env Invoke-EnvironmentChooser
+function Invoke-EnvironmentChooser {
+    $files = (dir ~/env).FullName | fzf -m
+    foreach ($line in (Get-Content $files | op inject -f)) {
+        if ($line[0] -in @($null, "#", " ", "`t")) { continue }
+        $parts = $line -split "=",2
+        Set-Content Env:\$($parts[0]) $parts[1]
+        Write-Host "Set environment variable: $($parts[0])"
+    }
+}
+
+New-Alias terraform Invoke-TerraformWithAutomaticOpen
+function Invoke-TerraformWithAutomaticOpen {
+    # .exe to avoid the alias below
+    terraform.exe $Args | foreach-object {
+        if ($_ -like "https://app.terraform.io/app/*/runs/run-*") {
+            $esc = [char]0x1B
+            $url = if ($_ -match "(.*)$esc") {
+                $Matches[1]
+            } else {
+                $_
+            }
+            Start-Process $url
+        }
+        $_
+    }
+}
+
+Add-Type @'
+    using System;
+    using System.Runtime.InteropServices;
+    public class WinAp {
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll")]
+        public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+    }
+'@
+
+function Invoke-ForegroundWindowChanges {
+    $prev = $null
+    while ($true) {
+        $h = [WinAp]::GetForegroundWindow()
+        if ($h -ne $prev) {
+            $p = Get-Process | ? { $_.MainWindowHandle -eq $h }
+            "{0:u} foreground window: {1}" -f (Get-Date), (@($p.Name) -join ",")
+            $prev = $h
+        }
+        Start-Sleep -milliseconds 10
+    }
+}
+
+New-Alias fg Invoke-ForegroundWindowChanges
+
+function Invoke-GatherWindows {
+    param($ProcessName = $null)
+    if ($ProcessName -eq $null) {
+        return
+    }
+
+    $hs = (Get-Process -Name $ProcessName | ? MainWindowTitle).MainWindowHandle
+    if ($hs -eq $null) { return }
+
+    $offset = 0
+    foreach ($h in $hs) {
+        [WinAp]::MoveWindow($h, $offset, $offset, 600, 400, $true)
+        [WinAp]::SetForegroundWindow($h)
+        $offset += 25
+    }
+}
+
+function Login-PodmanAcr {
+    param($Name = (Read-Host -Prompt "Registry name"))
+    $json = az acr login --name $Name --expose-token | ConvertFrom-Json
+    podman login $json.loginServer -u 00000000-0000-0000-0000-000000000000 -p $json.accessToken
+}
 
 Export-ModuleMember `
-    -Function Get-Sequence, Invoke-Xargs `
-    -Alias seq, xargs
+    -Function * `
+    -Alias *
