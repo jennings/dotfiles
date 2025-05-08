81a74779581fa11f78283aa43ddd53b72ba64692
Stephen Jennings <stephen.g.jennings@gmail.com>
2025-05-08 14:41:24 -0700

    remove hammerspoon

--- dot_hammerspoon/init.lua
+++
@@ -1,4 +0,0 @@
-j = {}
-
-j.mouse     = require "mouse"
-j.network   = require "network"
--- dot_hammerspoon/sudo.lua
+++
@@ -1,7 +0,0 @@
-local SUDO = "/usr/bin/sudo"
-
-function sudo(done, args)
-    return hs.task.new(SUDO, done, args)
-end
-
-return sudo
